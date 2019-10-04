32ff3c374d571fe87255e10b023814a1fd20ea42
Rina Schiller <schiller.rina@gmail.com>
2019-10-04 12:19:01 -0400

    hw1

---
+++ HW1/diffing.py
@@ -0,0 +1,89 @@
+# TODO: Your name, Cornell NetID
+# TODO: Your Partner's name, Cornell NetID
+
+import dynamic_programming
+
+# DO NOT CHANGE THIS CLASS
+class DiffingCell:
+    def __init__(self, s_char, t_char, cost):
+        self.cost = cost
+        self.s_char = s_char
+        self.t_char = t_char
+        self.validate()
+
+    # Helper function so Python can print out objects of this type.
+    def __repr__(self):
+        return "(%d,%s,%s)"%(self.cost, self.s_char, self.t_char)
+
+    # Ensure everything stored is the right type and size
+    def validate(self):
+        assert(type(self.cost) == int), "cost should be an integer"
+        assert(type(self.s_char) == str), "s_char should be a string"
+        assert(type(self.t_char) == str), "t_char should be a string"
+        assert(len(self.s_char) == 1), "s_char should be length 1"
+        assert(len(self.t_char) == 1), "t_char should be length 1"
+
+# Input: a dynamic programming table,  cell index i and j, the input strings s and t, and a cost function cost.
+# Should return a DiffingCell which we will place at (i,j) for you.
+def fill_cell(table, i, j, s, t, cost):
+    # If the index of the same letters are the same don't change it bc its 0
+    # add a dash at the beginning of each string to form the - table
+    s = "-" + s
+    t = "-" + t
+    # print i
+    # print j 
+    # print s 
+    # print t
+    print_table(table._table)
+    return DiffingCell(s[i], t[j] , costfunc(s[i], t[j]))
+
+# Input: n and m, represents the sizes of s and t respectively.
+# Should return a list of (i,j) tuples, in the order you would like fill_cell to be called
+def cell_ordering(n,m):
+    return [(a, b) for a in range(n+1) for b in range(m+1)]
+
+# Returns a size-3 tuple (cost, align_s, align_t).
+# cost is an integer cost.
+# align_s and align_t are strings of the same length demonstrating the alignment.
+# See instructions.pdf for more information on align_s and align_t.
+def diff_from_table(s, t, table):
+    # TODO: YOUR CODE HERE
+    return (0, "", "")
+
+def print_table(table):
+    print("-----")
+    for row in table:
+        print(row)    
+
+# Example usage
+if __name__ == "__main__":
+    # Example cost function from instructions.pdf
+    def costfunc(s_char, t_char):
+        if s_char == t_char: return 0
+        if s_char == 'a':
+            if t_char == 'b': return 5
+            if t_char == 'c': return 3
+            if t_char == '-': return 2
+        if s_char == 'b':
+            if t_char == 'a': return 1
+            if t_char == 'c': return 4
+            if t_char == '-': return 2
+        if s_char == 'c':
+            if t_char == 'a': return 5
+            if t_char == 'b': return 5
+            if t_char == '-': return 1
+        if s_char == '-':
+            if t_char == 'a': return 3
+            if t_char == 'b': return 3
+            if t_char == 'c': return 3
+
+    import dynamic_programming
+    s = "acb"
+    t = "baa"
+    D = dynamic_programming.DynamicProgramTable(len(s) + 1, len(t) + 1, cell_ordering(len(s), len(t)), fill_cell)
+    # print "table is " + D.get(0,1)
+    D.fill(s = s, t = t, cost=costfunc)
+    (cost, align_s, align_t) = diff_from_table(s,t, D)
+    print align_s
+    print align_t
+    print "cost was %d"%cost
